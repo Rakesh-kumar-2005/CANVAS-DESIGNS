88ead21a88697daa7e6ef317c322531ddea6127b
Rakesh Kumar Mohanty <mohantyrakesh802@gmail.com>
2025-08-28 19:36:17 +0530

    ....

--- hello/app/page.tsx
+++ hello/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      Hello World
-    </main>
+    <div style={{padding: '20px'}}>
+      <div className="bg-blue-500 text-white p-8 m-4">
+        <h1 className="text-4xl font-bold">Blue Background Test</h1>
+      </div>
+      <div className="bg-green-500 text-black p-8 m-4">
+        <h2 className="text-2xl">Green Background Test</h2>
+      </div>
+    </div>
   );
 }
